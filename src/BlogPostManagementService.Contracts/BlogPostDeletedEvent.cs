2e4767e5d554195f55b94c0011ec497c67675515
Alexander-Shein <alexa-montana@live.com>
2023-08-29 22:05:45 +0300

    Move to a separate repository

---
+++ src/BlogPostManagementService.Contracts/BlogPostDeletedEvent.cs
@@ -0,0 +1,15 @@
+﻿namespace BlogPostManagementService.Contracts
+{
+    public class BlogPostDeletedEvent : EventBase
+    {
+        public const string EventName = "BlogPostManagement.BlogPostDeleted";
+        public Guid BlogPostId { get; }
+        public string AuthorId { get; }
+
+        public BlogPostDeletedEvent(Guid blogPostId, string authorId, DateTime createdAt) : base (createdAt)
+        {
+            BlogPostId = blogPostId;
+            AuthorId = authorId;
+        }        
+    }
+}
